6e48e515f80339735ad94ef18028bb91cfc49388
omotosh <andrew.omotosho@gmail.com>
2019-05-30 14:26:48 -0400

    service config
    service config

--- src/main/java/org/orbitshakers/tra/TraApplication.java
+++ src/main/java/org/orbitshakers/tra/TraApplication.java
@@ -2,7 +2,12 @@ package org.orbitshakers.tra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages={
+		"org.orbitshakers.tra", 
+		"org.orbitshakers.tra.filter"
+})
 @SpringBootApplication
 public class TraApplication {
 
